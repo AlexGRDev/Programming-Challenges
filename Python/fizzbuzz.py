1eefd1bed3c97b9dea60bddc7d210e376f5fe48a
Alex Garcia Rodriguez <alexgaro2015@gmail.com>
2024-11-11 17:39:42 +0100

    correcion de fizzbuzz

--- Python/fizzbuzz.py
+++ Python/fizzbuzz.py
@@ -1,22 +1,25 @@
 def fizzbuzz(multiplo_3: int, multiplo_5: int) -> int:
-    i: int = 1
-    if isinstance(i and multiplo_3 and multiplo_5, int):
+    if (isinstance(multiplo_3 and multiplo_5, int)):
+        i: int 
+        i = 1
         while i <= 100:
-            if (i % multiplo_3 == "0" and i % multiplo_5 == 0):
+            if i % multiplo_3 == 0 and i % multiplo_5 == 0:
                 print("fizzbuzz")
-            elif (i % multiplo_3 == 0):
+            elif i % multiplo_3 == 0:
                 print("fizz")
-            elif (i % multiplo_5 == 0):
+            elif i % multiplo_5 == 0:
                 print("buzz")
             else:
                 print(i)
             i += 1
     else:
-        return ""
+        print("Los parámetros deben ser enteros.")
+
 
 def main() -> int:
     fizzbuzz(3, 5)
     return 0
-    
+
+
 if __name__ == "__main__":
     main()
