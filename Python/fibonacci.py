d983aecadfdd582fc3d1ca972dff80087ccf59fa
Alex Garcia Rodriguez <alexgaro2015@gmail.com>
2024-11-12 11:22:24 +0100

    fibonacci

--- Python/fibonacci.py
+++ Python/fibonacci.py
@@ -2,14 +2,13 @@ def fibonacci(a: int, b: int) -> int:
     if not (isinstance(a, int) and isinstance(b, int)):
         return (-1)
 
-    counter: int
-
     counter = 0
     while counter <= 50:
         print(a)
         a, b = b, a + b
         counter += 1
-    return (a) 
+    
+    return (a)
 
 def main() -> None:
     print(fibonacci(0, 1))
