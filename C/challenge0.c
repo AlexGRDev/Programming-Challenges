a41049d6581ce3293b935a5871eabfd134f587e7
AlexGRDev <alexgaro2015@gmail.com>
2024-12-08 21:00:46 +0100

    modified:   challenge0.c | Challenge finished

--- C/challenge0.c
+++ C/challenge0.c
@@ -6,3 +6,40 @@
     * - Múltiplos de 5 por la palabra "buzz".
     * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
 */
+
+#include <stdio.h>
+
+void fizzbuzz(int *num) 
+{
+    if (*num % 3 == 0 && *num % 5 == 0) 
+    {
+        printf("fizzbuzz\n");
+    }
+    else if (*num % 3 == 0)
+    {
+        printf("fizz\n");
+    }
+    else if (*num % 5 == 0)
+    {
+        printf("buzz\n");
+    } 
+    else 
+    {
+        printf("%d\n", *num);
+    }
+}
+
+int main() 
+{
+    int i;
+    int *ptrI = &i;
+
+
+    i = 1;
+    while (*ptrI <= 100) 
+    {
+        fizzbuzz(&i);
+        (*ptrI)++;
+    }
+    return 0;
+}
