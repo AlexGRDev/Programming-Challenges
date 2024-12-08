6fe82134cf45589c67c66f310fa4043aef8b4659
AlexGRDev <alexgaro2015@gmail.com>
2024-12-08 21:00:58 +0100

    modified:   challenge0\1.c | Challenge finished

--- C/challenge1.c
+++ C/challenge1.c
@@ -6,3 +6,63 @@
     * - NO hace falta comprobar que ambas palabras existan.
     * - Dos palabras exactamente iguales no son anagrama.
  */
+
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool anagram(char *word1, char *word2)
+{
+    if (strcmp(word1, word2) == 0)
+    {
+        return false;
+    }
+
+    if (strlen(word1) != strlen(word2))
+    {
+        return false;
+    }
+
+    int char_counter[256];
+    int i, j;
+
+    i = 0;
+    while (i < 256)
+    {
+        char_counter[i] = 0;
+
+        if (word1[i] != '\0')
+        {
+            char_counter[(unsigned char)(word1[i])]++;
+        }
+        j = 0;
+        while (j < i)
+        {
+            if (char_counter[j] != 0)
+            {
+                char_counter[(unsigned char)(word2[j])]++;
+            }
+            j++;
+        }
+        i++;
+    }
+
+    return true;
+}
+
+int main() 
+{
+    char word1[] = "pera";
+    char word2[] = "perra";
+
+    if (anagram(word1, word2))
+    {
+        printf("Las palabras son anagramas\n");
+    } 
+    else
+    {
+        printf("Las palabras no son anagramas\n");
+    }
+    return 0;
+}
