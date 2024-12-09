4cf3dc945763eca273c7d9f96b2323f04b595d33
AlexGARDev <alexgaro2015@ggmail.com>
2024-12-09 17:04:16 +0100

    modified:   challenge3.c | challenge 3 finished

--- C/challenge3.c
+++ C/challenge3.c
@@ -3,3 +3,37 @@
     * Hecho esto, imprime los números primos entre 1 y 100.
 */
 
+#include <stdio.h>
+#include <stdbool.h>
+
+bool es_primo(int *n)
+{
+    if (*n <= 1) return false;
+
+    int i = 2;
+    while (i <= *n / 2)
+    {
+        if (*n % i == 0) return false;
+        i++;
+    }
+    return true;
+}
+
+int main()
+{
+    int i;
+    int *ptrI = &i;
+
+    *ptrI = 1;
+    while (*ptrI <= 100)
+    {
+        if (es_primo(&i))
+        {
+            printf("%d\n", *ptrI);
+        }
+        (*ptrI)++;
+    }
+    return 0;
+}
+
+    
